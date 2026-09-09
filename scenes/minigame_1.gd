fb711b108c22cfc9be68dd511b6ef9598b39019a
sparklydinosaur310-design <sparklydinosaur310@gmail.com>
2026-09-09 21:30:53 +0800

    finished for today

--- scenes/minigame_1.gd
+++ scenes/minigame_1.gd
@@ -1,37 +1,28 @@
 extends Node2D
-@onready var themed_timer: Node2D = $ThemedTimer 
-# ^^^ You dragged this in the scene by the way 
+@onready var timer: RichTextLabel = $timer #literally just the richlabeltext
 
+var time : float
 
-
-var garlic_collected = 0 # just keeping track of garlic collected
-var timer_end = false # boolean (true or false) stating whether the timer ended
-
+# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	pass
 
-		#Below you can see that I have a function that I named. I grab a 
-		#function from it that was created in it's script and use `await` to 
-		# tell the script to wait for a signal, or for when a function finshes
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	timer.text = str(snapped(time, 0.10)) # this makes names easier
 
-
-	await themed_timer.Timer(10.0) #accessing a function from this node
-	#after this is compeleted...
-	timer_end = true # now we're saying "oh ye you ran out of time"
-
-func _process(delta: float) -> void: # running every frame brochacho
+func Timer(start_time: float): # making a new function for timer countdown!
+	# we want the timer to go down, and when it reaches 0 it transitions 
+	# to the next scene!
 	
-	if garlic_collected == 3: # the double equals is just an argument asking if it's the same, with "=" it'll give an error
-		if Global.minigames_done > 3: # we access a global script and see how many minigames have been compeleted
-			get_tree().change_scene_to_file("res://scenes/done_screen.tscn") # change current play scene into another, but you make your own finish screen in a later challenge, dont worry abt this rn
-		else:
-			get_tree().change_scene_to_file("res://scenes/timer_screen.tscn") # go back to the intermission scene
+	time = start_time
 	
-	if timer_end: # if the timer does end...
-		Global.minigames_done -=1 #go back a minigame
-		Global.lives -= 1 # lose ur lives
-		get_tree().change_scene_to_file("res://scenes/timer_screen.tscn") # back to intermission
-		
-
-func garlic_collect() -> void: # cool function that you connect to those garlics
-	garlic_collected = garlic_collected +1
+	while time > 0.0: # run if timer hasnt reached 0
+		await wait(0.10)
+		time = time - 0.10
+	
+	#when timer reaches 0
 	return
+	
+func wait(seconds: float) -> void: # write this simple function out for wait!
+	await get_tree().create_timer(seconds).timeout # makes u wait
--- scenes/title_screen.gd
+++ scenes/title_screen.gd
@@ -7,13 +7,10 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
-
-
-func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/level_screen.tscn")
 
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/level_screen.tscn")
+	print("start pressed")
