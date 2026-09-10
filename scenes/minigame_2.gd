a061ad3bb369a371e8d96c80f743d1b058622cc0
sparklydinosaur310-design <sparklydinosaur310@gmail.com>
2026-09-10 23:08:29 +0800

    finished minigame2?

--- scenes/minigame_2.gd
+++ scenes/minigame_2.gd
@@ -1,23 +1,40 @@
 extends Node2D
-@onready var themed_timer: Node2D = $ThemedTimer
+@onready var timer: RichTextLabel = $Timer
 
-var buttons_pressed := 0
-var timer_end = false
+var time: float
+var garlics_clicked := 0
 
 func _ready() -> void:
-	await themed_timer.Timer(7.0)
-	#after this is completed...
-	timer_end = true 
-
+	Timer(15.0)
 
 func _process(delta: float) -> void:
-	if buttons_pressed == 4:
-		if Global.minigames_done > 3:
-			get_tree().change_scene_to_file("res://scenes/done_screen.tscn")
-		else:
-			get_tree().change_scene_to_file("res://scenes/timer_screen.tscn")
-	
-	if timer_end:
-		Global.lives -= 1
-		Global.minigames_done -=1
-		get_tree().change_scene_to_file("res://scenes/timer_screen.tscn")
+	timer.text = str(snapped(time, 0.10))
+
+func Timer(start_time: float):
+	time = start_time
+	while time > 0.0:
+		await wait(0.10)
+		time -= 0.10
+	get_tree().call_deferred("change_scene_to_file", "res://scenes/lose_screen.tscn")
+
+func wait(seconds: float) -> void:
+	await get_tree().create_timer(seconds).timeout
+
+func _on_button_1_pressed() -> void:
+	$Button_1.hide()
+	garlics_clicked += 1
+	check_win()
+
+func _on_button_2_pressed() -> void:
+	$Button_2.hide()
+	garlics_clicked += 1
+	check_win()
+
+func _on_button_3_pressed() -> void:
+	$Button_3.hide()
+	garlics_clicked += 1
+	check_win()
+
+func check_win() -> void:
+	if garlics_clicked >= 3:
+		get_tree().call_deferred("change_scene_to_file", "res://scenes/win_screen.tscn")
